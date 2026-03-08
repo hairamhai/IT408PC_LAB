2b5f6dd79e7474584197cb06f484ae8cfcc47f15
hairamhai <saketh.3545@gmail.com>
2026-03-08 19:07:09 +0530

    Add LAB1P3.java with variable declarations and output

---
+++ LAB-1/LAB1P3.java
@@ -0,0 +1,15 @@
+class L1P3{
+	public static void main(String[] args){
+		byte by=7;
+		boolean bo=true;
+		short s=2;
+		long l=4;
+		int a=10;
+		float b=2.8f;
+		char ch='a';
+		double d=98.56;
+		String e="Hello";
+		System.out.println("\nbyte value:"+by+"\nboolean value:"+bo+"\nshort value:"+s+"\ninteger value:"+a+"\nlong value:"+l+"\nfloat value:"+b+"\nchar :"+ch+"\ndouble value:"+d+"\nString value:"+e);
+	}
+
+}
